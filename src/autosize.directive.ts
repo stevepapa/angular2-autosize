464de5919623b540ec86a8a1007387976db04541
papa <steve@nift>
2016-02-09 20:24:47 +1100

    Add overflow hidden to create cleaner height

--- src/autosize.directive.ts
+++ src/autosize.directive.ts
@@ -15,7 +15,8 @@ export class Autosize {
     this.adjust();
   }
   adjust(){
-	this.element.nativeElement.style.height = 'auto';
+    this.element.nativeElement.style.overflow = 'hidden';
+    this.element.nativeElement.style.height = 'auto';
     this.element.nativeElement.style.height = this.element.nativeElement.scrollHeight + "px";
   }
 }
