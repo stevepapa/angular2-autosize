309412974e2d38ebb6e4b02a17202455ccc9c3d8
Stefan Mohr <smohr@resiliencesoftware.com>
2016-05-31 10:12:36 -0700

    Updated ng import from @angular/core Declared types for function return types and parameters

--- src/autosize.directive.ts
+++ src/autosize.directive.ts
@@ -1,4 +1,4 @@
-import { ElementRef, HostListener, Directive} from 'angular2/core';
+import { ElementRef, HostListener, Directive} from '@angular/core';
 
 @Directive({
     selector: 'textarea[autosize]'
@@ -6,15 +6,15 @@ import { ElementRef, HostListener, Directive} from 'angular2/core';
 
 export class Autosize {
  @HostListener('input',['$event.target'])
-  onInput(textArea) {
+  onInput(textArea: HTMLTextAreaElement): void {
     this.adjust();
   }
   constructor(public element: ElementRef){
   }
-  ngOnInit(){
+  ngOnInit(): void{
     this.adjust();
   }
-  adjust(){
+  adjust(): void{
     this.element.nativeElement.style.overflow = 'hidden';
     this.element.nativeElement.style.height = 'auto';
     this.element.nativeElement.style.height = this.element.nativeElement.scrollHeight + "px";
