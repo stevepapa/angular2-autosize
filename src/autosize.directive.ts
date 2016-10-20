b512ccb6c61d6ea4f27ff7d7ceb3612f3f4755b7
eswar-s <eswar.s@jeanmartin.us>
2016-10-21 00:41:11 +0530

    fix bug - page couldn't be scrolled to the end due to textarea height changes in ngAfterContentChecked

--- src/autosize.directive.ts
+++ src/autosize.directive.ts
@@ -1,22 +1,25 @@
-import { ElementRef, HostListener, Directive} from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, SimpleChange, Renderer } from '@angular/core';
 
 @Directive({
     selector: 'textarea[autosize]'
 })
-
 export class Autosize {
- @HostListener('input',['$event.target'])
-  onInput(textArea: HTMLTextAreaElement): void {
+  @Input() autosize: string;
+  
+  constructor(public renderer: Renderer, public element: ElementRef) {
+      this.renderer.setElementAttribute(this.element.nativeElement, 'rows', '1');
+  }
+
+  ngOnChanges(changes: {[key: string]: SimpleChange}) {
     this.adjust();
   }
-  constructor(public element: ElementRef){
-  }
-  ngAfterContentChecked(): void{
-    this.adjust();
-  }
+  
   adjust(): void{
-    this.element.nativeElement.style.overflow = 'hidden';
-    this.element.nativeElement.style.height = 'auto';
-    this.element.nativeElement.style.height = this.element.nativeElement.scrollHeight + "px";
+    this.renderer.setElementStyle(this.element.nativeElement, 'overflow', 'hidden');
+    this.renderer.setElementStyle(this.element.nativeElement, 'height', 'auto');
+    let scrollHeight = this.element.nativeElement.scrollHeight;
+    if(scrollHeight > 0) {
+      this.renderer.setElementStyle(this.element.nativeElement, 'height', scrollHeight + 'px');
+    }
   }
 }
